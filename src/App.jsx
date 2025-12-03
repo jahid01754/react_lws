9c5d58db17960e0b1e906bb77ae5957b4ca84b02
Md. Jahidul Hasan <jahidulhasan01754@gmail.com>
2025-12-03 14:53:25 +0600

    composition vs inheritance

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Clock from "./components/Clock";
 import ClockList from "./components/ClockList";
 import Form from "./components/Form";
 import FormPractise from "./components/FormPractise";
+// import Text from "./components/inheritance/Text";
+import Text from "./components/composition/text";
+import Emoji from "./components/composition/Emoji";
+import Bracket from "./components/composition/Bracket";
 
 function App() {
   // const quantities = [1, 2, 3];
@@ -13,8 +17,18 @@ function App() {
       {/* <ClockList quantity={quantities} /> */}
       {/* <Form /> */}
       {/* <FormPractise /> */}
-      <Calculator />
-      <CalculatorP />
+      {/* <Calculator /> */}
+      {/* <CalculatorP /> */}
+      {/* <Text /> */}
+      <Emoji>
+        {({ addEmoji }) => (
+          <Bracket>
+            {({ addBracket }) => (
+              <Text addEmoji={addEmoji} addBracket={addBracket} />
+            )}
+          </Bracket>
+        )}
+      </Emoji>
     </>
   );
 }
